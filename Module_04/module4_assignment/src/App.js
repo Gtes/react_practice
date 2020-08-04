84587b8f1ad21b1ec84c03a3a7e40e8d13e683ac
Gtes <gates1993@gmail.com>
2020-08-04 15:45:32 +0300

    assigment initial template

---
+++ Module_04/module4_assignment/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+import "./App.css";
+
+const App = (props) => {
+
+    return (
+        <div className="App">
+
+        </div>
+    );
+};
+
+export default App;
