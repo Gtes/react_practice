897371373d0b09ba044a416b4b8cba49513037de
Gtes <gates1993@gmail.com>
2020-08-07 14:10:54 +0300

    11. CSS Modules & Media Queries

--- my-app/src/Person/Person.js
+++ my-app/src/Person/Person.js
@@ -1,22 +1,10 @@
 import React from "react";
-import styled from "styled-components";
 
-const StyleDiv = styled.div`
-    width: 60%;
-    margin: 10px auto;
-    border: 1px solid #eeeeee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
-
-    @media (min-width: 500px) {
-        width: 450px;
-    }
-`;
+import classes from "./Person.module.css"
 
 const person = (props) => {
     return (
-        <StyleDiv>
+        <div className={classes.Person}>
             <p onClick={props.click}>
                 I'm {props.name} and I am {props.age} years old!
             </p>
@@ -26,7 +14,7 @@ const person = (props) => {
                 onChange={props.changed}
                 defaultValue={props.name}
             />
-        </StyleDiv>
+        </div>
     );
 };
 
