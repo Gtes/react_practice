78652fbc4ac5982eb5b626301b636d2ca20abd7e
Gtes <gates1993@gmail.com>
2020-08-12 22:36:20 +0300

    24. Passing Unknown Props

--- my-app/src/components/Persons/Person/Person.js
+++ my-app/src/components/Persons/Person/Person.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Aux from "../../../hoc/Aux";
 
+import withClass from '../../../hoc/withClass'
+
 import classes from "./Person.module.css";
 
 class Person extends Component {
@@ -24,4 +26,4 @@ class Person extends Component {
     }
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
